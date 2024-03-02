e15948d99f1cfc04ab8e57c7c173c705f763f2e8
dariametelitsa <dariametelitsa@gmail.com>
2024-03-02 14:07:32 +0300

    Comments was deleted

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import { MenuData } from '../../layout/header/Header';
 
-// type MenuPropsType = {
-//   // items: { item: string; link: string; }[],
-//   items: Array<Menus>,
-// }
 
 export const Menu = (props: {items: Array<MenuData>}) => {
   return (
