a1128f568b7837b2435b7f3458d362a76b58ed05
dariametelitsa <dariametelitsa@gmail.com>
2024-03-04 11:27:47 +0300

    Added footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,13 +1,64 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Icon } from '../../components/icon/Icon';
+import { FlexWrapper } from '../../components/FlexWrapper';
 
 export const Footer = () => {
   return (
     <StyledFooter>
+      <FlexWrapper direction='column' alignI='center'>
+        <Name>Svetlana</Name>
+        <SocialList>
+          <SocialItem>
+            <SocialLink>
+              <Icon iconId='inst' width={'21px'} />
+            </SocialLink>
+          </SocialItem>
 
+          <SocialItem>
+            <SocialLink>
+              <Icon iconId='telegr' width={'21px'} />
+            </SocialLink>
+          </SocialItem>
+
+          <SocialItem>
+            <SocialLink>
+              <Icon iconId='vk' width={'21px'} />
+            </SocialLink>
+          </SocialItem>
+
+          <SocialItem>
+            <SocialLink>
+              <Icon iconId='in' width={'21px'} />
+            </SocialLink>
+          </SocialItem>
+        </SocialList>
+        <Copiright>© 2023 Svetlana Dyablo, All Rights Reserved.</Copiright>
+      </FlexWrapper>
     </StyledFooter>
   );
 };
 
 const StyledFooter = styled.footer `
+  background-color: #e09f88;
+  min-height: 20vh;
+`
+
+const Name = styled.h5 `
+`
+
+const SocialList = styled.ul `
+  display: flex;
+  gap: 10px;
+  padding-left: 0;
+`
+
+const SocialItem = styled.li `
+  list-style-type: none;
+`
+
+const SocialLink = styled.a `
+`
+
+const Copiright = styled.small `
 `
