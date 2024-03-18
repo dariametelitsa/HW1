f57e771007d42924557d03e514d2bcef6095ea22
dariametelitsa <dariametelitsa@gmail.com>
2024-03-18 18:34:20 +0300

    Testimony mobile was done

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -5,18 +5,21 @@ import { Icon } from '../../../components/icon/Icon';
 import { Slider } from '../../../components/slider/Slider';
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import { IconWrapper } from "../skills/skill/Skill";
+import { Container } from "../../../components/Container";
 
 export const Testimony = () => {
   return (
     <StyledTestimony>
-      <SectionTitle>Testimony</SectionTitle>
-      <FlexWrapper direction='column' alignI='center'>
-        <IconWrapper>
-          <Icon iconId={'quote'}/>
-        </IconWrapper>
-        <Slider/>
+      <Container>
+        <SectionTitle>Testimony</SectionTitle>
+        <FlexWrapper direction='column' alignI='center'>
+          <IconWrapper>
+            <Icon iconId={'quote'}/>
+          </IconWrapper>
+          <Slider/>
 
-      </FlexWrapper>
+        </FlexWrapper>
+      </Container>
     </StyledTestimony>
   );
 };
