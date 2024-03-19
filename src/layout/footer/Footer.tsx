a1305132035d693a87611552d814161e5f7cd2f2
dariametelitsa <dariametelitsa@gmail.com>
2024-03-19 12:07:04 +0300

    Footer were refactored

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,87 +1,43 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Icon } from '../../components/icon/Icon';
 import { FlexWrapper } from '../../components/FlexWrapper';
-import { theme } from "../../styles/Theme";
-import { font } from "../../styles/Common";
+import { S } from "./Footer_Styles";
 
-export const Footer = () => {
+
+const SocialItemData = [
+  {
+    iconID: 'inst'
+  },
+  {
+    iconID: 'telegr'
+  },
+  {
+    iconID: 'vk'
+  },
+  {
+    iconID: 'in'
+  },
+]
+
+export const Footer:React.FC = () => {
   return (
-    <StyledFooter>
+    <S.Footer>
       <FlexWrapper direction='column' alignI='center'>
-        <Name>Svetlana</Name>
-        <SocialList>
-          <SocialItem>
-            <SocialLink>
-              <Icon iconId='inst' width={'21px'} height={'21px'} />
-            </SocialLink>
-          </SocialItem>
-
-          <SocialItem>
-            <SocialLink>
-              <Icon iconId='telegr' width={'21px'} height={'21px'} />
-            </SocialLink>
-          </SocialItem>
-
-          <SocialItem>
-            <SocialLink>
-              <Icon iconId='vk' width={'21px'} height={'21px'} />
-            </SocialLink>
-          </SocialItem>
-
-          <SocialItem>
-            <SocialLink>
-              <Icon iconId='in' width={'21px'} height={'21px'} />
-            </SocialLink>
-          </SocialItem>
-        </SocialList>
-        <Copyright>© 2023 Svetlana Dyablo, All Rights Reserved.</Copyright>
+        <S.Name>Svetlana</S.Name>
+        <S.SocialList>
+          {
+            SocialItemData.map((s,index) => {
+              return <li>
+                <S.SocialLink>
+                  <Icon iconId={s.iconID} width={'21px'} height={'21px'} />
+                </S.SocialLink>
+              </li>
+            })
+          }
+        </S.SocialList>
+        <S.Copyright>© 2023 Svetlana Dyablo, All Rights Reserved.</S.Copyright>
       </FlexWrapper>
-    </StyledFooter>
+    </S.Footer>
   );
 };
 
-const StyledFooter = styled.footer `
-  background-color: ${theme.colors.primaryBg};
-  padding: 40px 0;
-`
-
-const Name = styled.h5 `
-  ${font({family: "'Josefin Sans', sans-serif", weight: 700, Fmin: 16, Fmax: 22})}
-  letter-spacing: 3px;
-`
-
-const SocialList = styled.ul `
-  display: flex;
-  gap: 20px;
-  margin: 30px 0;
-`
-
-const SocialItem = styled.li `
-
-`
-
-const SocialLink = styled.a `
-  background-color: rgba(255, 255, 255, 0.1);
-  border-radius: 50%;
-  width: 35px;
-  height: 35px;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  color: ${theme.colors.accent};
-
-  &:hover {
-    color: ${theme.colors.primaryBg};
-    transform: translateY(-4px);
-  }
-`
-
-const Copyright = styled.small `
-  font-weight: 400;
-  font-size: 12px;
-  text-align: center;
-  opacity: 0.5;
-`
