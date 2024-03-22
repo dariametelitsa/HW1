73dacf95dae3b83629d360e79fe908e726656d4a
dariametelitsa <dariametelitsa@gmail.com>
2024-03-22 18:08:56 +0300

    added scroll animations

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -4,6 +4,7 @@ import { SectionTitle } from '../../../components/SectionTitle';
 import { Skill } from './skill/Skill';
 import { Container } from "../../../components/Container";
 import { S } from "./Skills_Styles";
+import { Fade } from "react-awesome-reveal";
 
 export const Skills = () => {
   return (
@@ -11,13 +12,18 @@ export const Skills = () => {
       <Container>
         <SectionTitle>My Skills</SectionTitle>
         <FlexWrapper as='ul' wrap justify='space-between'>
-          {
-            skillData.map((s, index) => {
-              return <Skill iconId={s.iconId} key={index}
-                            title={s.title}
-                            description={s.description} />
-            })
-          }
+          <Fade
+            cascade={true}
+            damping={0.2}>
+            {
+              skillData.map((s, index) => {
+                return <Skill iconId={s.iconId} key={index}
+                              title={s.title}
+                              description={s.description}/>
+              })
+            }
+          </Fade>
+
         </FlexWrapper>
       </Container>
     </S.Skills>
